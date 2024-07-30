e4009b2b9b72cb5b7105006a765c900f0257c4f2
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-07-30 15:31:39 +0530

    changes

--- src/Newimg.jsx
+++ src/Newimg.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 
 const Newimg = (props) => {
-
     // eslint-disable-next-line react/prop-types
-    const imgapi = `https://source.unsplash.com/900x450/?${props.name}`
+    const imgapi = `https://source.unsplash.com/?${props.name}`
     return(
         <>
             <img className="imgset" src={imgapi} alt="" />
+            {/* <div>{imgapi}</div> */}
         </>
     )
 }
-
 export default Newimg
